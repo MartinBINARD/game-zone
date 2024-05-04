49a252f09428e781fd08b7622c134f309984eac1
MartinBINARD <martin.binard.dev@gmail.com>
2024-05-03 16:51:33 -1000

    Add rating images

--- screens/reviewDetails.js
+++ screens/reviewDetails.js
@@ -1,16 +1,32 @@
-import { View, Text } from 'react-native';
-import { globalStyles } from '../styles/global';
+import { View, Text, Image, StyleSheet } from 'react-native';
+import { globalStyles, images } from '../styles/global';
 import Card from '../shared/card';
 
 export default function ReviewDetails({ route }) {
   const { title, body, rating } = route.params;
+
   return (
     <View style={globalStyles.container}>
       <Card>
         <Text>{title}</Text>
         <Text>{body}</Text>
         <Text>{rating}</Text>
+        <View style={styles.rating}>
+          <Text>GameZone rating : </Text>
+          <Image source={images.ratings[rating]} />
+        </View>
       </Card>
     </View>
   );
 }
+
+const styles = StyleSheet.create({
+  rating: {
+    flexDirection: 'row',
+    justifyContent: 'center',
+    paddingTop: 16,
+    marginTop: 16,
+    borderTopWidth: 1,
+    borderTopColor: '#eee',
+  },
+});
