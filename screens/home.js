babe0f7dd616be8b0b142a4276e1eabdfe0ae147
MartinBINARD <martin.binard.dev@gmail.com>
2024-05-06 15:57:24 -1000

    Create addReview and pass it to reviewForm

--- screens/home.js
+++ screens/home.js
@@ -6,6 +6,8 @@ import {
   TouchableOpacity,
   Modal,
   StyleSheet,
+  TouchableWithoutFeedback,
+  Keyboard,
 } from 'react-native';
 import { MaterialIcons } from '@expo/vector-icons';
 import { globalStyles } from '../styles/global';
@@ -16,7 +18,7 @@ import ReviewForm from './reviewForm';
 export default function Home({ navigation }) {
   const [modalOpen, setModalOpen] = useState(false);
 
-  const [reviews] = useState([
+  const [reviews, setReviews] = useState([
     {
       title: 'Zelda, Breath of Fresh Air',
       rating: 5,
@@ -37,18 +39,28 @@ export default function Home({ navigation }) {
     },
   ]);
 
+  const addReview = (review) => {
+    review.key = Math.random().toString();
+    setReviews((currentReview) => {
+      return [review, ...currentReview];
+    });
+    setModalOpen(false);
+  };
+
   return (
     <View style={globalStyles.container}>
       <Modal visible={modalOpen} animationType="slide">
-        <View style={styles.modalContent}>
-          <MaterialIcons
-            name="close"
-            size={24}
-            style={{ ...styles.modalToggle, ...styles.modalClose }}
-            onPress={() => setModalOpen(false)}
-          />
-          <ReviewForm />
-        </View>
+        <TouchableWithoutFeedback onPress={Keyboard.dismiss}>
+          <View style={styles.modalContent}>
+            <MaterialIcons
+              name="close"
+              size={24}
+              style={{ ...styles.modalToggle, ...styles.modalClose }}
+              onPress={() => setModalOpen(false)}
+            />
+            <ReviewForm addReview={addReview} />
+          </View>
+        </TouchableWithoutFeedback>
       </Modal>
 
       <MaterialIcons
