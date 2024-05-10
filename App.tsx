4e37d708a00a83a8f74d62205b129cceb197999e
MartinBINARD <martin.binard.dev@gmail.com>
2024-05-09 17:35:38 -1000

    Add App & tsconfig.json

--- App.tsx
+++ App.tsx
@@ -23,7 +23,7 @@ export default function App() {
         getFonts();
         // Artificially delay for two seconds to simulate a slow loading
         // experience. Please remove this if you copy and paste the code!
-        await new Promise((resolve) => setTimeout(resolve, 2000));
+        await new Promise<void>((resolve) => setTimeout(resolve, 2000));
       } catch (e) {
         console.warn(e);
       } finally {
