8efaae82d732cda27298739bb90189d56e05bfb1
MartinBINARD <martin.binard.dev@gmail.com>
2024-05-09 18:19:34 -1000

    Install babel-plugin-module-resolver

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,13 @@
-module.exports = function(api) {
-  api.cache(true);
-  return {
-    presets: ['babel-preset-expo'],
-  };
+module.exports = {
+  presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        root: ['.'],
+        extensions: ['.ios.js', '.android.js', '.js', '.ts', '.tsx', '.json'],
+      },
+    ],
+    'react-native-reanimated/plugin',
+  ],
 };
